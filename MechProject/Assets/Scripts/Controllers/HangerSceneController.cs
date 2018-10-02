1af1db556b831b4a6446ef53d0879d18addab0cb
buttception <spartandevilexdreme@gmail.com>
2018-10-02 12:45:09 +0800

    Minor edits
    +hanger scene eye light and pulsating

---
+++ MechProject/Assets/Scripts/Controllers/HangerSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangerSceneController : MonoBehaviour {
+
+    public GameObject eyeLight;
+    float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > 1f)
+        {
+            timer = 0f;
+            eyeLight.SetActive(!eyeLight.activeSelf);
+        }
+	}
+
+    public void ChangeToNextScene()
+    {
+
+    }
+}
