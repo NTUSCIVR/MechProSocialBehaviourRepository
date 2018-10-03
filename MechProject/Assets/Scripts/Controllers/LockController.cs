ac482cbc32b3191ca16557e1706410d502c80429
buttception <spartandevilexdreme@gmail.com>
2018-10-03 11:41:04 +0800

    got rings that can touch now

---
+++ MechProject/Assets/Scripts/Controllers/LockController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test");
+        if(other.tag == "Controller")
+        {
+            if(other.gameObject == MainSceneController.instance.LeftController)
+            {
+                if(!MainSceneController.instance.leftAttached)
+                {
+                    MainSceneController.instance.leftAttached = true;
+                    //attach to left hand
+                    MainSceneController.instance.AttachRing(other.gameObject);
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                if(!MainSceneController.instance.rightAttached)
+                {
+                    MainSceneController.instance.rightAttached = true;
+                    //attach to right hand
+                    MainSceneController.instance.AttachRing(other.gameObject);
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Test");
+    }
+}
---
+++ MechProject/Assets/Scripts/Controllers/MainSceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneController : MonoBehaviour {
+
+    public static MainSceneController instance;
+
+    public GameObject LeftController;
+    public GameObject RightController;
+
+    public GameObject blueCirclePrefab;
+
+    public bool leftAttached = false;
+    public bool rightAttached = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AttachRing(GameObject go)
+    {
+        Instantiate(blueCirclePrefab, go.transform);
+    }
+}
