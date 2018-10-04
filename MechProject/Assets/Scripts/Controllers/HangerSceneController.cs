dcb543500269395601a555cc88f54ffd4c181c77
Shih Wei <loneswei@gmail.com>
2018-10-04 16:23:16 +0800

    Added text Panel in Hanger(start) scene

--- MechProject/Assets/Scripts/Controllers/HangerSceneController.cs
+++ MechProject/Assets/Scripts/Controllers/HangerSceneController.cs
@@ -1,25 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class HangerSceneController : MonoBehaviour {
+public class HangerSceneController : MonoBehaviour
+{
 
     public GameObject eyeLight;
-    float timer = 0f;
+    public Text TextObject;
+    public string TextToShow = "This is a default sentence.";
+    public float DisplayRate = 0.1f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private float timer = 0.0f;
+    private string TextThatIsShowing = "";
+    
 	// Update is called once per frame
-	void Update () {
-        timer += Time.deltaTime;
-        if(timer > 1f)
+	void Update ()
+    {
+        if (TextToShow.Length >= 1)
         {
-            timer = 0f;
-            eyeLight.SetActive(!eyeLight.activeSelf);
+            timer += Time.deltaTime;
+            if (timer >= DisplayRate)
+            {
+                // Robot Eye Blinks
+                eyeLight.SetActive(!eyeLight.activeSelf);
+
+                // Add First Character of TextToShow into the Text UI
+                TextThatIsShowing += TextToShow[0];
+                TextToShow = TextToShow.Remove(0, 1);
+                TextObject.text = TextThatIsShowing;
+                timer = 0.0f;
+            }
         }
+        else
+            eyeLight.SetActive(false);
 	}
 
     public void ChangeToNextScene()
