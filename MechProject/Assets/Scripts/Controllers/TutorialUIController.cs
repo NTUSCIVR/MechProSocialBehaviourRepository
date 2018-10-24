25f3d7fddba91945ccc35263e7ba974b20df523d
buttception <spartandevilexdreme@gmail.com>
2018-10-24 09:47:28 +0800

    Minor edits

--- MechProject/Assets/Scripts/Controllers/TutorialUIController.cs
+++ MechProject/Assets/Scripts/Controllers/TutorialUIController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TutorialUIController : MonoBehaviour {
 
+    public GameObject leftArrow;
+    public GameObject rightArrow;
+    public GameObject forwardBackwardArrow;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,23 @@ public class TutorialUIController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!MainSceneController.instance.swipeLeftBefore)
+        {
+            leftArrow.SetActive(true);
+        }
+        else if(!MainSceneController.instance.swipeRightBefore)
+        {
+            leftArrow.SetActive(false);
+            rightArrow.SetActive(true);
+        }
+        else if(!MainSceneController.instance.moveBefore)
+        {
+            rightArrow.SetActive(false);
+            forwardBackwardArrow.SetActive(true);
+        }
+        else
+        {
+            forwardBackwardArrow.SetActive(false);
+        }
 	}
 }
